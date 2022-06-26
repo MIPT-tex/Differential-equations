6b3d3c79b219b9e739c4b3e7e1838b8b2651e099
Pavel Filippenko <rubikpf2002@mail.ru>
2022-06-26 11:42:26 +0300

    Add build instructions

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	pdflatex Differential_equations.tex
+
+clean:
+	rm *.aux *.fdb_latexmk *.fls *.out *.gz *.log *.bbl *.blg *.toc
--- clean.sh
+++ clean.sh
@@ -1,1 +1,35 @@
-rm *.aux *.fdb_latexmk *.fls *.out *.gz *.log *.toc
+#!/bin/bash
+
+aux=Differential_equations.aux
+fdb_latexmk=Differential_equations.fdb_latexmk
+fls=Differential_equations.fls
+out=Differential_equations.out
+gz=Differential_equations.gz
+log=Differential_equations.log
+bbl=Differential_equations.bbl
+blg=Differential_equations.blg
+toc=Differential_equations.toc
+
+arr=($aux $fdb_latexmk $fls $out $gz $log $bbl $blg $toc)
+
+for i in ${!arr[@]}; do
+    if [ -f ${arr[$i]} ]
+    then
+    echo "${arr[$i]} exists"
+    rm ${arr[$i]}
+    else
+    echo "${arr[$i]} doesn't exists"
+    fi
+done
+
+dir=.vscode
+
+if [ -e $dir ]
+then
+echo "$dir exists"
+rm -r $dir
+else
+echo "$dir doesn't exists"
+fi
+
+echo "all technical files were removed"
